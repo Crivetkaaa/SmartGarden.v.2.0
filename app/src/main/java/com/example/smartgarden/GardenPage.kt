0ec4de4f0e3addf024846f7b949526092ae94e7d
Ratmir Khasanov <khasanovri@mail.ru>
2025-03-01 19:36:57 +0300

    Сделана страница теплицы, сделаны заделы под редактирование теплицы и ардуино

--- app/src/main/java/com/example/smartgarden/GardenPage.kt
+++ app/src/main/java/com/example/smartgarden/GardenPage.kt
@@ -5,22 +5,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.smartgarden.ui.theme.SmartGardenTheme
 
 class GardenPage : ComponentActivity() {
@@ -43,27 +67,54 @@ class GardenPage : ComponentActivity() {
 fun GardenPagePainter(modifier: Modifier = Modifier) {
     val context = LocalContext.current
 
-    var garden_data = remember { mutableStateOf<GardenArduino?>(null) }
+    var garden_infos by remember { mutableStateOf<GardenArduino?>(null) }
 
-    ConnectToAPI().getGardenFromAPI {  }
+    LaunchedEffect(false) {
+        getGardenInfo(context) { result ->
+            garden_infos = result
+        }
+    }
 
     Column(modifier.fillMaxSize()) {
         TopMenu(
             context,
             "Меню теплицы",
             "backArrow",
-            Icons.Default.ArrowBack,
+            Icons.AutoMirrored.Filled.ArrowBack,
             { backArrowButton(context) },
             true
         )
-        Text("vfbfdbfdb")
-        Row(modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically){
-            Text("Меню теплицы")
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(6.dp),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Text("Данные о теплице", fontSize = 26.sp)
         }
 
+        garden_infos?.garden?.let { gardenInfo ->
+            GardenMenuInfo("Теплица",
+                gardenInfo.garden_name,
+                gardenInfo.garden_description,
+                { editButtonGarden(context) })
+        }
+        garden_infos?.arduino?.let { arduinoInfo ->
+            GardenMenuInfo("Aрдуино",
+                arduinoInfo.arduino_name,
+                arduinoInfo.arduino_description,
+                { editButtonArduino(context) })
+        }
+
+        Row(
+            Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center
+        ) {
+            Text("Графики", fontSize = 20.sp)
+        }
+            //TODO() Дожелать графики
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
